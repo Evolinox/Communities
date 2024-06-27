7485339cfa15d07651e34183cc788d3938fa22d6
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-06-28 01:00:33 +0200

    added styling

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,23 @@
 </script>
 
 <template>
-  <Sidebar />
-  <div>
+  <Sidebar id="sidebar"/>
+  <div class="contentview">
     <RouterView />
   </div>
 </template>
+
+<style>
+  #sidebar {
+    width: 70px;
+  }
+
+  .contentview {
+    height: 100%;
+    width: calc(100% - 70px);
+    position: fixed;
+    left: 70px;
+    top: 0;
+    background-color: rgb(40,40,40);
+  }
+</style>
